a0b08a760c70cc50076e97a87c57d45fbeeb3e7d
Farelito <bakoufarel@gmail.com>
2024-05-10 15:31:04 +0200

    tiens

--- debugging/factorial_recursive.py
+++ debugging/factorial_recursive.py
@@ -3,34 +3,19 @@ import sys
 
 def factorial(n):
     """
-    Calculate the factorial of a given number using recursion.
-
     Function Description:
-    ----------------------
-    This function calculates the factorial of a non-negative integer using recursion.
-    The factorial of a non-negative integer n, denoted as n!, is the product of all positive integers less than or equal to n.
+    Calculates the factorial of a given integer.
 
     Parameters:
-    -----------
-    n : int
-        The non-negative integer for which the factorial is to be calculated.
+    n (int): The integer for which the factorial is to be calculated.
 
     Returns:
-    --------
-    int
-        The factorial of the input integer n.
+    int: The factorial of the input integer.
     """
-    if n == 0:  # Base case: If n is 0, return 1
+    if n == 0:
         return 1
     else:
-        # Recursive case: Multiply n with the factorial of (n-1)
         return n * factorial(n-1)
 
-# Get the input integer from the command line argument
-input_integer = int(sys.argv[1])
-
-# Calculate the factorial of the input integer using the factorial function
-result = factorial(input_integer)
-
-# Print the factorial of the input integer
-print(result)
+f = factorial(int(sys.argv[1]))
+print(f)
